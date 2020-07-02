8d84e7a283978ed7ff2dba02f79b929d03d4a4da
janikmiet <jani.k.miettinen@gmail.com>
2020-07-02 11:06:08 +0300

    read classes changes

--- R/read_classes_csv.R
+++ R/read_classes_csv.R
@@ -1,16 +1,15 @@
-#' Read classes definitions from csv file
+#' Read class definitions from csv file
 #'
-#' Reads classes definitions based on the file path and operates classification definitions from wide format to long format.
+#' Use this function for your own code classification. It reads class definition file based on the file path and operates classification definitions from wide format to long format. You can edit you own classifications in Spreadsheet Program (ex. LibreOffice, Excel) and save file to csv-file.
 #'
-#' @family help functions
-#' @param file the filename or path to the filename as a character string.
-#' 
+#' @family read functions
+#' @param file filename or file path as a character string.
 #' @return returns classification definintion in long format (tidy data) as that is what the package needs in operation.
-#'
 #' @importFrom vroom vroom
-#' 
 #' @examples
-#' elixhauser_cl <- read_classes_csv(file = "data/classification_codes/elixhauser_classes_wide.csv")
+#'  \dontrun{
+#' my_classes <- read_classes_csv(file = "my_classes.csv")
+#' }
 #' @rdname read_classes_csv
 #' @export
 #' 
